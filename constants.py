8c9730de81289166e82339c9775e66188596d039
JohannesChopov <johannes.chopov@student.uhasselt.be>
2024-05-08 16:01:49 +0200

    setup

---
+++ constants.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed May  8 15:51:52 2024
+
+@author: Johannes
+"""
+# useful constants for program
---
+++ run.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed May  8 15:51:35 2024
+
+@author: Johannes
+"""
+
+import constants as c
+
+def run():
+    return 0
+
+if __name__ == '__main__':
+    run()
+    
